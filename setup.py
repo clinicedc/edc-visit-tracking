29afc0e92a3e81dd185b133f646ed7a2aba7919d
erikvw <ew2789@gmail.com>
2019-01-24 21:46:21 -0600

    tox

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
     VERSION = f.read()
 
+tests_require = []
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    for line in f:
+        tests_require.append(line.strip())
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -47,4 +52,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    python_requires=">=3.7",
+    tests_require=tests_require,
+    test_suite='runtests.main',
 )
