0f851890b5aa312282d742ce7a95f184cef8a237
erikvw <ew2789@gmail.com>
2019-07-20 11:57:15 -0500

    admin list display/search/fields

--- edc_visit_tracking/modeladmin_mixins/crf_model_admin_mixin.py
+++ edc_visit_tracking/modeladmin_mixins/crf_model_admin_mixin.py
@@ -6,32 +6,51 @@ class CrfModelAdminMixin:
 
     date_hierarchy = "report_datetime"
 
+    def visit_reason(self, obj=None):
+        return getattr(obj, self.visit_model_attr).reason
+
+    def visit_code(self, obj=None):
+        return getattr(obj, self.visit_model_attr).appointment.visit_code
+
+    def subject_identifier(self, obj=None):
+        return getattr(obj, self.visit_model_attr).subject_identifier
+
     def get_list_display(self, request):
-        list_display = super().get_list_display(request)
-        list_display = list(list_display)
-        list_display.append(self.visit_model_attr)
-        return list_display
+        super().get_list_display(request)
+        fields = [self.visit_code, self.visit_reason]
+        fields_first = [self.subject_identifier, "report_datetime"]
+        self.list_display = list(self.list_display)
+        try:
+            self.list_display.remove("__str__")
+        except ValueError:
+            pass
+        self.list_display = (
+            [f for f in fields_first if f not in self.list_display]
+            + self.list_display
+            + [f for f in fields if f not in self.list_display]
+        )
+        return self.list_display
 
     def get_search_fields(self, request):
-        search_fields = super().get_search_fields(request)
-        search_fields = list(search_fields)
-        search_fields.extend(
-            [f"{self.visit_model_attr}__appointment__subject_identifier"]
+        super().get_search_fields(request)
+        fields = [f"{self.visit_model_attr}__appointment__subject_identifier"]
+        self.search_fields = [f for f in fields if f not in self.search_fields] + list(
+            self.search_fields
         )
-        return search_fields
+        return self.search_fields
 
     def get_list_filter(self, request):
-        list_filter = super().get_list_filter(request)
-        list_filter = list(list_filter)
-        list_filter.extend(
-            [
-                f"{self.visit_model_attr}__report_datetime",
-                f"{self.visit_model_attr}__reason",
-                f"{self.visit_model_attr}__appointment__appt_status",
-                f"{self.visit_model_attr}__appointment__visit_code",
-            ]
-        )
-        return list_filter
+        super().get_list_filter(request)
+        fields = [
+            f"{self.visit_model_attr}__report_datetime",
+            f"{self.visit_model_attr}__appointment__visit_code",
+            f"{self.visit_model_attr}__reason",
+            f"{self.visit_model_attr}__appointment__appt_status",
+        ]
+        self.list_filter = list(self.list_filter) + [
+            f for f in fields if f not in self.list_filter
+        ]
+        return self.list_filter
 
     @property
     def visit_model(self):
