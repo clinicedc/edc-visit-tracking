8f7dc0f8568df416857e928f62934671f404ee26
erikvw <ew2789@gmail.com>
2020-06-17 16:44:39 -0500

    use get_visit_model

--- edc_visit_tracking/model_mixins/__init__.py
+++ edc_visit_tracking/model_mixins/__init__.py
@@ -1,9 +1,5 @@
 from .caretaker_fields_mixin import CaretakerFieldsMixin
 from .crfs import VisitTrackingCrfModelMixin, CrfInlineModelMixin
 from .previous_visit_model_mixin import PreviousVisitModelMixin, PreviousVisitError
-from .subject_visit_missed_model_mixin import (
-    SubjectVisitMissedModelMixin,
-    SUBJECT_VISIT_MISSED_MODEL,
-    SUBJECT_VISIT_MISSED_REASONS_MODEL,
-)
+from .subject_visit_missed_model_mixin import SubjectVisitMissedModelMixin
 from .visit_model_mixin import VisitModelMixin, VisitModelFieldsMixin
