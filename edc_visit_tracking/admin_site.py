220243fa9eb760a0c1ccea4b8bde2b5ca55fd58a
erikvw <ew2789@gmail.com>
2021-01-24 22:12:01 -0600

    update for django 3.1

--- edc_visit_tracking/admin_site.py
+++ edc_visit_tracking/admin_site.py
@@ -1,12 +1,10 @@
-from django.contrib.admin import AdminSite
+from edc_model_admin.admin_site import EdcAdminSite
 
 
-class EdcVisitTrackingAdminSite(AdminSite):
+class AdminSite(EdcAdminSite):
     site_header = "Edc Visit Tracking"
     site_title = "Edc Visit Tracking"
     index_title = "Edc Visit Tracking Administration"
-    site_url = "/administration/"
 
 
-edc_visit_tracking_admin = EdcVisitTrackingAdminSite(name="edc_visit_tracking_admin")
-edc_visit_tracking_admin.disable_action("delete_selected")
+edc_visit_tracking_admin = AdminSite(name="edc_visit_tracking_admin")
